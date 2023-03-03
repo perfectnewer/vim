6dc4cedec32bc53eb93923b3a83564d73e071f25
simon <xp.wei@idiaoyan.com>
2023-03-03 21:19:08 +0800

    fix(format): lsp config

--- lua/pl_lsp.lua
+++ lua/pl_lsp.lua
@@ -6,8 +6,8 @@ local function register(use)
   use {
     'ms-jpq/coq_nvim', branch = 'coq',
     requires = {
-      {'ms-jpq/coq.artifacts', branch = 'artifacts'},
-      {'ms-jpq/coq.thirdparty', branch = '3p'},
+      { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
+      { 'ms-jpq/coq.thirdparty', branch = '3p' },
     },
     config = function()
       vim.g.coq_settings = {
@@ -20,7 +20,7 @@ local function register(use)
         },
         display = {
           icons = {
-            mode =  'none'
+            mode = 'none'
           }
         },
         keymap = {
@@ -30,33 +30,32 @@ local function register(use)
     end,
     -- ft = {'python', 'lua', 'go'}
   }
-  use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
+  use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
 
   use({
     'glepnir/lspsaga.nvim',
     branch = 'main',
     requires = {
       'neovim/nvim-lspconfig',
-      {'ms-jpq/coq_nvim', branch = 'coq'},
-      {'ms-jpq/coq.artifacts', branch = 'artifacts'},
+      { 'ms-jpq/coq_nvim', branch = 'coq' },
+      { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
     },
     config = function()
-      vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
       -- vim.lsp.set_log_level('debug')
-      local opts = { noremap=true, silent=true }
+      local opts = { noremap = true, silent = true }
       vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
       -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
       -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
       vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
       local saga = require('lspsaga')
       saga.setup({
-          -- your configuration
+        -- your configuration
       })
 
       vim.keymap.set("n", "<Leader>ot", "<cmd>Lspsaga open_floaterm zsh<CR>", { silent = true })
       vim.keymap.set("t", "<Leader>ot", "<C-\\><C-n><cmd>Lspsaga close_floaterm<CR>", { silent = true })
 
-      vim.keymap.set('n', '<Leader>ol', '<cmd>Lspsaga outline <CR>', {silent = true})
+      vim.keymap.set('n', '<Leader>ol', '<cmd>Lspsaga outline <CR>', { silent = true })
       vim.keymap.set('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', { silent = true })
       vim.keymap.set('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', { silent = true })
       -- Only jump to error
@@ -71,11 +70,12 @@ local function register(use)
       -- after the language server attaches to the current buffer
       local on_attach = function(client, bufnr)
         -- Enable completion triggered by <c-x><c-o>
+        vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
         vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
         -- Mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
         vim.keymap.set('n', 'gt', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', bufopts)
@@ -108,17 +108,17 @@ local function register(use)
         -- This is the default in Nvim 0.7+
         debounce_text_changes = 150,
       }
-      require'lspconfig'.solargraph.setup{
-          on_attach = on_attach,
-          flags = lsp_flags,
+      require 'lspconfig'.solargraph.setup {
+        on_attach = on_attach,
+        flags = lsp_flags,
       }
-      require'lspconfig'.tsserver.setup{
-          on_attach = on_attach,
-          flags = lsp_flags,
+      require 'lspconfig'.tsserver.setup {
+        on_attach = on_attach,
+        flags = lsp_flags,
       }
-      require'lspconfig'.gopls.setup{
-          on_attach = on_attach,
-          flags = lsp_flags,
+      require 'lspconfig'.gopls.setup {
+        on_attach = on_attach,
+        flags = lsp_flags,
       }
 
       local util = require('lspconfig/util')
@@ -140,30 +140,34 @@ local function register(use)
         return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
       end
 
-      require'lspconfig'.pyright.setup(require('coq').lsp_ensure_capabilities({
-          on_attach = on_attach,
-          flags = lsp_flags,
-          on_init = function(client)
-              client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
-          end,
-        })
+      -- only show above warnings
+      -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+      -- capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+      require 'lspconfig'.pyright.setup(require('coq').lsp_ensure_capabilities({
+        --  capabilities = capabilities,
+        on_attach = on_attach,
+        flags = lsp_flags,
+        on_init = function(client)
+          client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
+        end,
+      })
       )
-      require('lspconfig')['rust_analyzer'].setup{
-          on_attach = on_attach,
-          flags = lsp_flags,
-          -- Server-specific settings...
-          settings = {
-            ['rust-analyzer'] = {}
-          }
+      require('lspconfig')['rust_analyzer'].setup {
+        on_attach = on_attach,
+        flags = lsp_flags,
+        -- Server-specific settings...
+        settings = {
+          ['rust-analyzer'] = {}
+        }
       }
 
-      require'lspconfig'.lua_ls.setup(require('coq').lsp_ensure_capabilities({
+      require 'lspconfig'.lua_ls.setup(require('coq').lsp_ensure_capabilities({
         on_attach = on_attach,
         settings = {
           Lua = {
             diagnostics = {
               -- Get the language server to recognize the `vim` global
-              globals = {'vim'},
+              globals = { 'vim' },
             },
             workspace = {
               -- Make the server aware of Neovim runtime files
@@ -172,12 +176,12 @@ local function register(use)
           },
         },
         cmd = {
-          vim.fn.stdpath('config')..'/lua-language-server/bin/lua-language-server'
+          vim.fn.stdpath('config') .. '/lua-language-server/bin/lua-language-server'
         }
       }))
       vim.cmd('COQnow')
     end,
-    ft = {'python', 'lua', 'go'},
+    ft = { 'python', 'lua', 'go' },
   })
 
   use({
@@ -187,14 +191,14 @@ local function register(use)
       vim.opt.listchars:append 'eol:↴'
       -- vim.opt.listchars:append 'space:⋅'
       require('indent_blankline').setup({
-          space_char_blankline = ' ',
-          show_current_context = true,
-          show_current_context_start = false,
-          show_trailing_blankline_indent = false,
-          show_end_of_line = true,
+        space_char_blankline = ' ',
+        show_current_context = true,
+        show_current_context_start = false,
+        show_trailing_blankline_indent = false,
+        show_end_of_line = true,
       })
     end,
-    ft = {'python', 'go', 'lua', 'js'}
+    ft = { 'python', 'go', 'lua', 'js' }
   })
 
   use 'SirVer/ultisnips'
